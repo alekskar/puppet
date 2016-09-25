34c63411356131ea96fdcd7de9fcc7e7dd37a038
alekskar <aliaksandr_karavai@epam.com>
2016-09-25 20:52:35 +0300

    Update mem.rb

--- lib/facter/mem.rb
+++ lib/facter/mem.rb
@@ -1,10 +1,13 @@
 Facter.add(:mem_to_allocate) do
   setcode do
-    if Facter.value(:memorysize_mb) < 1024
+#    if Facter.value(:memorysize_mb) < 1024
+     if $::memorysize_mb < 1024
       '512m'
-    elsif Facter.value(:memorysize_mb) < 2048
+#    elsif Facter.value(:memorysize_mb) < 2048
+     elsif $::memorysize_mb < 2048
       '1g'
-    elsif Facter.value(:memorysize_mb) < 2048
+#    elsif Facter.value(:memorysize_mb) < 4048
+     elsif $::memorysize_mb < 4096
       '2g'
     else
       '4g'
