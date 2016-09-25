67ea7f48842f42c9f5c0561e8543e62c7c418252
alekskar <aliaksandr_karavai@epam.com>
2016-09-25 23:00:58 +0300

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,13 +1,13 @@
 # this claas handled setup puppet on client
 class puppet::client inherits puppet {
-  package { 'puppet':
+  package { 'puppet-agent':
     ensure => $puppet::puppet_agent_version,
   }
   notify {"Puppet client at ${::hostname} with ${::puppet_agent_version} has been installed": }
   service { 'puppet':
     ensure  => 'running',
     enable  => true,
-    require => Package['puppet'],
+    require => Package['puppet-agent'],
   }
   notify { "Puppet client at ${::hostname} has been installed": }
 }
